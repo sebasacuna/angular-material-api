2a631ff141f8070186e8b2260e423591972a069b
sebasacuna <sebastianacuna.it@gmail.com>
2020-01-29 16:36:02 -0300

    add new url to cross origin

--- src/main/java/com/angular/api/forms/wsrest/WRestElements.java
+++ src/main/java/com/angular/api/forms/wsrest/WRestElements.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"http://app.com:4200","http://localhost:4200"}, maxAge = 3600)
+@CrossOrigin(origins = {"https://angular-material-forms.herokuapp.com","http://localhost:4200"}, maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class WRestElements {
